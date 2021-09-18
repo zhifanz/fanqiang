3392d31254150b295fc74677a752621dac145292
Zhifan Zhang <zzhang27@jaguarlandrover.com>
2021-09-18 14:14:27 +0800

    feat: apply runnel based proxy for clash client

--- lib/domain/Clash.ts
+++ lib/domain/Clash.ts
@@ -9,7 +9,15 @@ export function generateConfigFrom(endpoints: TunnelProxyConnectionInfo): string
   return yaml.stringify({
     port: 7890,
     "socks-port": 7891,
-    mode: "global",
+    "redir-port": 7892,
+    "tproxy-port": 7893,
+    "mixed-port": 7890,
+    mode: "rule",
+    dns: {
+      enable: true,
+      listen: "0.0.0.0:53",
+      "enhanced-mode": "redir-host",
+    },
     proxies: [
       {
         name: "shadowsocks",
@@ -20,5 +28,6 @@ export function generateConfigFrom(endpoints: TunnelProxyConnectionInfo): string
         password: endpoints.password,
       },
     ],
+    rules: ["DOMAIN-SUFFIX,google.com,auto", "DOMAIN,ad.com,REJECT", "GEOIP,CN,DIRECT", "MATCH,auto"],
   });
 }
