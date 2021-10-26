5dffb24b74fa031bf3c7d9e3f494ea0275148934
zhifanz <zhifan.zhang.1985@gmail.com>
2021-10-26 15:44:59 +0800

    ci: add python support in pipeline

--- analysis/tests.py
+++ analysis/tests.py
@@ -32,38 +32,9 @@ class TestMain(unittest.TestCase):
         self.assertTrue(domain_available('baidu.com'))
         self.assertFalse(domain_available('unknown.domain'))
 
-    def test_yaml(self):
-        content = """
-        k1: v1
-        k2: v2
-        """
-        result = yaml.load(content, Loader=yaml.FullLoader)
-        self.assertEqual('v1', result['k1'])
-        self.assertEqual('v2', result['k2'])
-
     def test_yaml_dump_array(self):
         output = yaml.dump({'payload': ['d1', 'd2']})
         self.assertEqual(output, 'payload:\n- d1\n- d2\n')
-
-    def test_yaml_read_file(self):
-        fp = tempfile.NamedTemporaryFile(mode='w')
-        fp.write("""
-        a: 1
-        b: c
-        """)
-        fp.flush()
-        with open(fp.name, 'r') as f:
-            result = yaml.load(f, Loader=yaml.FullLoader)
-        self.assertEqual(1, result['a'])
-        self.assertEqual('c', result['b'])
-
-    def test_yaml_write_file(self):
-        fp = tempfile.NamedTemporaryFile(mode='w')
-        yaml.dump({'a': 1}, fp)
-        fp.flush()
-        with open(fp.name, 'r') as f:
-            self.assertEqual('a: 1', f.read().strip())
-
 
 if __name__ == '__main__':
     unittest.main()
